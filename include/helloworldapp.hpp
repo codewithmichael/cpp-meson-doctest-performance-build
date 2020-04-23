3caf49035baa33762e4c56e4773907cacb2cd18a
Michael Spencer <code.with.michael@gmail.com>
2020-04-23 15:38:38 -0400

    Split single source file into headers and individual sources

---
+++ include/helloworldapp.hpp
@@ -0,0 +1,18 @@
+// helloworldapp.hpp
+
+
+#ifndef helloworldapp_hpp
+#define helloworldapp_hpp
+
+
+#include "wx.hpp"
+
+
+class HelloWorldApp : public wxApp
+{
+public:
+    virtual bool OnInit();
+};
+
+
+#endif //helloworldapp_hpp
--- src/helloworldapp.cpp
+++ src/helloworldapp.cpp
@@ -1,45 +1,8 @@
 // helloworldapp.cpp
 
 
-// For compilers that support precompilation, includes "wx/wx.h".
-#include <wx/wxprec.h>
-#ifndef WX_PRECOMP
-    #include <wx/wx.h>
-#endif
-
-
-class HelloWorldApp : public wxApp
-{
-public:
-    virtual bool OnInit();
-};
-
-
-class HelloWorldFrame : public wxFrame
-{
-public:
-    HelloWorldFrame(const wxString& title, const wxPoint& pos, const wxSize& size);
-
-private:
-    void OnHello(wxCommandEvent& event);
-    void OnExit(wxCommandEvent& event);
-    void OnAbout(wxCommandEvent& event);
-
-    wxDECLARE_EVENT_TABLE();
-};
-
-
-enum
-{
-    ID_Hello = 1
-};
-
-
-wxBEGIN_EVENT_TABLE(HelloWorldFrame, wxFrame)
-    EVT_MENU(ID_Hello, HelloWorldFrame::OnHello)
-    EVT_MENU(wxID_EXIT, HelloWorldFrame::OnExit)
-    EVT_MENU(wxID_ABOUT, HelloWorldFrame::OnAbout)
-wxEND_EVENT_TABLE()
+#include "helloworldapp.hpp"
+#include "helloworldframe.hpp"
 
 
 wxIMPLEMENT_APP(HelloWorldApp);
@@ -51,47 +14,3 @@ bool HelloWorldApp::OnInit()
     frame->Show();
     return true;
 }
-
-
-HelloWorldFrame::HelloWorldFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
-    : wxFrame(NULL, wxID_ANY, title, pos, size)
-{
-    wxMenu* menuFile = new wxMenu;
-    menuFile->Append(ID_Hello, "&Hello...\tCtrl-H",
-        "Help string shown in status bar for this menu item");
-    menuFile->AppendSeparator();
-    menuFile->Append(wxID_EXIT);
-
-    wxMenu* menuHelp = new wxMenu;
-    menuHelp->Append(wxID_ABOUT);
-
-    wxMenuBar* menuBar = new wxMenuBar;
-    menuBar->Append(menuFile, "&File");
-    menuBar->Append(menuHelp, "&Help");
-
-    SetMenuBar(menuBar);
-
-    CreateStatusBar();
-    SetStatusText("Welcome to wxWidgets!");
-}
-
-
-void HelloWorldFrame::OnExit(wxCommandEvent& event)
-{
-    Close();
-}
-
-
-void HelloWorldFrame::OnAbout(wxCommandEvent& event)
-{
-    wxMessageBox("This is a wxWidgets' Hello World sample",
-        "About Hello World", wxOK | wxICON_INFORMATION);
-}
-
-
-void HelloWorldFrame::OnHello(wxCommandEvent& event)
-{
-    wxLogMessage("Hello World from wxWidgets!");
-}
-
-
